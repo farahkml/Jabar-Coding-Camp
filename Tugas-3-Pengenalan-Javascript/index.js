90698e56cb446d96ee646c5c362bf21dc83759c5
farahkml <farahkamila30@gmail.com>
2021-09-01 20:14:31 +0700

    pengenalan javascript

---
+++ Tugas-3-Pengenalan-Javascript/index.js
@@ -0,0 +1,30 @@
+// soal 1
+var pertama = "saya sangat senang hari ini";
+var kedua = "belajar javascript itu keren";
+
+var pertama = "saya senang ";
+var kedua = "belajar JAVASCRIPT";
+console.log(pertama.concat(kedua)); 
+
+// soal 2
+var number1 = "10";
+var number2 = "2";
+var number3 = "4";
+var number4 = "6";
+
+console.log(number1 % number2 + number3 * number4);
+
+// soal 3
+var kalimat = 'wah javascript itu keren sekali'; 
+
+var kataPertama = kalimat.substring(0,3); 
+var kataKedua = kalimat.substring(4,14);
+var kataKetiga = kalimat.substring(15,18);
+var kataKeempat = kalimat.substring(19,24); 
+var kataKelima = kalimat.substring(25,31); 
+
+console.log(kataPertama); 
+console.log(kataKedua); 
+console.log(kataKetiga); 
+console.log(kataKeempat); 
+console.log(kataKelima);
